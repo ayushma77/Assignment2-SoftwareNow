74f7c81225a9b3919cec877c50f9de3e535846a7
ayushma <pandeyayushma77@gmail.com>
2026-03-28 12:00:06 +0930

    Setting assignment 2 structure

---
+++ question1.py
@@ -0,0 +1,8 @@
+"""
+Create a program that read the text file " raw_text.txt",
+encrypts its contents using a simple encryption method, 
+and writes the encrypted text into a new file "encrypted_text.txt". 
+Then create a function to decrypt the content and a function to 
+verify the decryption was successful.
+
+"""
